7e56f5b89085eb548c4c0c841cb2b1166bb69b16
Gowri V Gopal <gowrivgopal123@gmail.com>
2026-07-16 12:54:33 +0530

    Clear URL hash on page load so site always starts at top

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'   
 
+// Clear hash on fresh load so page always starts at top
+if (window.location.hash) {
+  history.replaceState(null, '', window.location.pathname)
+}
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
